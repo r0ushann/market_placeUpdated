679678dd52f09d343aeb1db10457660572ca9657
r0ushann <roushan007kr@gmail.com>
2023-01-18 11:58:44 +0530

    uploaded a marketplace

---
+++ hardhat.config.js
@@ -0,0 +1,21 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
+
+module.exports = {
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {},
+        // mumbai: {
+        //     url: https://polygon-mumbai.g.alchemy.com/v2/BoBVblJEM1aYN7X_bRvUlz40JQqRc2uZ,
+        //     accounts: [process.env.PRIVATE_KEY]
+        // }
+    },
+  solidity: "0.8.4",
+  
+  paths: {
+    artifacts: "./src/backend/artifacts",
+    sources: "./src/backend/contracts",
+    cache: "./src/backend/cache",
+    tests: "./src/backend/test"
+  },
+};
